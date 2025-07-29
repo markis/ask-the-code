a04b847bf2d17bf524e98fc71ef1f6cb730a0c02
Markis Taylor <m@rkis.net>
2025-07-29 07:02:57 -0400

    feat: suppress warnings and set env defaults
    - Set TOKENIZERS_PARALLELISM=false to avoid parallelism warnings
    - Set TRANSFORMERS_VERBOSITY=error to reduce transformer logs
    - Filter torch FutureWarnings and encoder attention mask warnings
    - Filter XLMRobertaTokenizerFast deprecation warnings

--- src/ask_the_code/__main__.py
+++ src/ask_the_code/__main__.py
@@ -1,3 +1,13 @@
+import os
+import warnings
+
+os.environ.setdefault("TOKENIZERS_PARALLELISM", "false")
+os.environ.setdefault("TRANSFORMERS_VERBOSITY", "error")
+
+warnings.filterwarnings("ignore", category=FutureWarning, module="torch")
+warnings.filterwarnings("ignore", message=".*encoder_attention_mask.*deprecated.*")
+warnings.filterwarnings("ignore", message=".*XLMRobertaTokenizerFast.*__call__.*method.*faster.*")
+
 from ask_the_code.cli import run
 
 run()
